34ee7144cc42e4e7a41247c1a5e7bd581339f8de
Kyle Wilcox <kwilcox@asascience.com>
2012-03-20 13:30:43 -0400

    Fix MODIS run script

--- modis/runMODISComposites.sh
+++ modis/runMODISComposites.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/data/remote_sensing/scripts/makeComposite.sh 1 /data/remote_sensing/SST /data/remote_sensing/masked /data/remote_sensing/composites/1-day
-/data/remote_sensing/scripts/makeComposite.sh 3 /data/remote_sensing/SST /data/remote_sensing/masked /data/remote_sensing/composites/3-day
-/data/remote_sensing/scripts/makeComposite.sh 7 /data/remote_sensing/SST /data/remote_sensing/masked /data/remote_sensing/composites/7-day
+/data/modis/scripts/makeMODISComposites.sh 1 /data/modis/raw /data/modis/composites/1-day
+/data/modis/scripts/makeMODISComposites.sh 3 /data/modis/raw /data/modis/composites/3-day
+/data/modis/scripts/makeMODISComposites.sh 7 /data/modis/raw /data/modis/composites/7-day
