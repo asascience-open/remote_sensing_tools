c38a21a29364552ccdd2682ac258f84010966827
Kyle Wilcox <kwilcox@asascience.com>
2012-12-04 17:27:26 -0500

    Configurable day for backprocessing SST

--- avhrr/backprocessDirectory.sh
+++ avhrr/backprocessDirectory.sh
@@ -8,8 +8,9 @@ echo "Backprocessing SST masks/composites from $2..."
 # Abolute Location of RAW data is second
 # Folder to put masked in is third
 # Folder to put composite in is fourth
+# How far back to generate composites for.  So the entire directory isn't processed
 
-for f in `find $2 -type f`; do
+for f in `find $2 -mtime -${5} -type f`; do
   # 20110801.213.1350.n16.EC1.nc
   fileDt=$(echo $f | sed 's/\([0-9]\{8\}\)\.\([0-9]\{3\}\)\.\([0-9]\{2\}\)\([0-9]\{2\}\)\.n\([0-9]\{2\}\)\.EC1\.nc/\1\ \3\:\4:00/' | awk -F"/" '{print $NF}')
   fileD=$(date --utc -d "$fileDt" +%Y%m%d.%H%M)
@@ -20,5 +21,6 @@ for f in `find $2 -type f`; do
     DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     bash $DIR/makeSSTComposite.sh $1 $f $3 $4
     #bash $DIR/justMask.sh $1 $f $3 $4
+    #echo "Does not exist: $fileD"
   fi
 done
--- avhrr/runSSTComposites.sh
+++ avhrr/runSSTComposites.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/data/remote_sensing/scripts/backprocessDirectory.sh 1 /data/remote_sensing/SST /data/remote_sensing/masked /data/remote_sensing/composites/1-day
-/data/remote_sensing/scripts/backprocessDirectory.sh 3 /data/remote_sensing/SST /data/remote_sensing/masked /data/remote_sensing/composites/3-day
-/data/remote_sensing/scripts/backprocessDirectory.sh 7 /data/remote_sensing/SST /data/remote_sensing/masked /data/remote_sensing/composites/7-day
+/data/remote_sensing/scripts/backprocessDirectory.sh 1 /data/remote_sensing/SST /data/remote_sensing/masked /data/remote_sensing/composites/1-day 30
+/data/remote_sensing/scripts/backprocessDirectory.sh 3 /data/remote_sensing/SST /data/remote_sensing/masked /data/remote_sensing/composites/3-day 30
+/data/remote_sensing/scripts/backprocessDirectory.sh 7 /data/remote_sensing/SST /data/remote_sensing/masked /data/remote_sensing/composites/7-day 30
