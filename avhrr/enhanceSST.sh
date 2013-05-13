7959d049ed7aba6b78096d4e4ba182517b1bd317
Kyle Wilcox <kwilcox@asascience.com>
2013-05-13 12:13:14 -0400

    Update comments section of cloud_mask variable for SST files

--- avhrr/enhanceSST.sh
+++ avhrr/enhanceSST.sh
@@ -93,20 +93,25 @@ ncatted -h -a grid_mapping,mcsst,o,c,"crs" $1
 
 # Remove unneeded attributes on the "mcsst" variable
 ncatted -h \
-	-a Equator_Radius,mcsst,d,, \
-	-a Projection,mcsst,d,, \
-	-a Center_Latitude,mcsst,d,, \
-	-a Center_Longitude,mcsst,d,, \
-	-a Flattening,mcsst,d,, \
-	-a comments,mcsst,o,c,"Projection information found in the crs variable" \
-	$1
+  -a Equator_Radius,mcsst,d,, \
+  -a Projection,mcsst,d,, \
+  -a Center_Latitude,mcsst,d,, \
+  -a Center_Longitude,mcsst,d,, \
+  -a Flattening,mcsst,d,, \
+  -a comments,mcsst,o,c,"Projection information found in the crs variable" \
+  $1
+  
+# enhance comments section of the clould masking test
+ncatted -h \
+  -a comments,cloud_land_mask,o,c,"The land mask flag is 99; Missing values from the TerraScan software have a flag of zero; A measurement not on land and not missing from the TerraScan software fails the time test if it is more than 2 degrees cooler than the mean of the satellite passes in the previous 72 hrs; A measurement fails the climatology test if the temperature is more than 5 degrees colder than the climatologies calculated by NSIPP AVHRR Pathfinder and Erosion Global 9km SST Climatology (Casey, Cornillon) A description of this climatology can be found at ftp://podaac.jpl.nasa.gov/pub/documents/dataset_docs/nsipp_climatology.htm; The combined time and climatology test is not a simple combination of a failure of the time and climatology tests. In this test, the previous 72 hours or satellite passes are first passed through a climatology test, then passed through the time test. It is a sequential test, rather than two independent tests. If any measurement is fails a test, all neighboring measurements within 3km are aslo automatically failed. The flag for the time test, climatoiloty test, and sequential test are 1, 2 and 4 respectively. The integer sum of these tests tell the user which of the three tests any pixel failed. Interpretation of the flags are; 0 = passed all tests 0+0+0; 1 = failed only time test 1+0+0=1; 2 = failed only climatology test 0+2+0=2; 3 = failed time test and climatology test 1+2+0=3; 4 = failed only sequential test test  0+0+4=4; 5 = failed time test and sequential test  1+0+4=5; 6 = failed climatology test and sequential test 0+2+4=6; 7 = failed time test, climatology test and sequential test 1+2+4=7. Alternatively; Pixels that failed the time test could have flags of 1, 3, 4, 7; Pixels that failed the climatology test could have flags of 2, 3, 6, 7; Pixels that failed the sequential test could have flags of 4, 5, 6, 7" \
+  $1
 
 # Add standard names
 ncatted -h \
-	-a standard_name,lon,o,c,"longitude" \
-	-a standard_name,lat,o,c,"latitude" \
-	-a standard_name,time,o,c,"time" \
-	-a standard_name,mcsst,o,c,"sea_surface_temperature" \
-	$1
+  -a standard_name,lon,o,c,"longitude" \
+  -a standard_name,lat,o,c,"latitude" \
+  -a standard_name,time,o,c,"time" \
+  -a standard_name,mcsst,o,c,"sea_surface_temperature" \
+  $1
 
 echo "Complete."
